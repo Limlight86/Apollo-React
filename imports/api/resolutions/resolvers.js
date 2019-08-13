287df983a5d2cf818e8321139857c8e20e98f46d
Lazaro <lquin008@gmail.com>
2019-08-13 15:56:00 -0400

    added Form and Mutation connection

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -1,13 +1,17 @@
 import Resolutions from "./resolutions";
 
-Resolutions.insert({
-  name: "test Res"
-});
-
 export default {
   Query: {
     resolutions() {
       return Resolutions.find({}).fetch();
+    }
+  },
+  Mutation: {
+    createResolution() {
+      console.log("got here");
+      //   const resolutionId = Resolutions.insert({
+      //     name: "test Res"
+      //   });
     }
   }
 };
