d3dfae24fcbde8c2a9c855aafd4af0488923347f
Lazaro <lquin008@gmail.com>
2019-08-16 16:46:49 -0400

    moved queries extended and cleaned testSchema

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -2,29 +2,13 @@ import { createApolloServer } from "meteor/apollo";
 import { makeExecutableSchema } from "graphql-tools";
 import merge from "lodash/merge";
 import ResolutionsSchema from "../../api/resolutions/Resolutions.graphql";
-import UsersSchema from '../../api/users/User.graphql'
+import UsersSchema from "../../api/users/User.graphql";
 import ResolutionResolvers from "../../api/resolutions/resolvers";
 import UsersResolvers from "../../api/users/resolvers";
 
-const testSchema = `
-type Query {
-  hi: String
-  resolutions: [Resolution]
-  user: User
-}
-`;
+const typeDefs = [ResolutionsSchema, UsersSchema];
 
-const typeDefs = [testSchema, ResolutionsSchema, UsersSchema];
-
-const testResolvers = {
-  Query: {
-    hi() {
-      return "Hello Level Up";
-    }
-  }
-};
-
-const resolvers = merge(testResolvers, ResolutionResolvers, UsersResolvers);
+const resolvers = merge(ResolutionResolvers, UsersResolvers);
 
 const schema = makeExecutableSchema({
   typeDefs,
