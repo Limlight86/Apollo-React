ba6ac629bd3ab9f509d447493a959e6ff55a2947
Lazaro <lquin008@gmail.com>
2019-08-17 17:16:38 -0400

    mutation user validation in place

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -13,18 +13,21 @@ export default {
       const goals = Goals.find({
         resolutionId: resolution._id
       }).fetch();
-      if (goals.length === 0) return false
+      if (goals.length === 0) return false;
       const completedGoals = goals.filter(goal => goal.completed);
       return completedGoals.length === goals.length;
     }
   },
   Mutation: {
     createResolution(obj, { name }, { userId }) {
-      const resolutionId = Resolutions.insert({
-        name,
-        userId
-      });
-      return Resolutions.findOne(resolutionId);
+      if (userId) {
+        const resolutionId = Resolutions.insert({
+          name,
+          userId
+        });
+        return Resolutions.findOne(resolutionId);
+      }
+      throw new Error("Unauthorized");
     }
   }
 };
