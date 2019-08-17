793a062ffc8781bfa00e8e8de54a0b250128630a
Lazaro <lquin008@gmail.com>
2019-08-17 15:53:41 -0400

    resolutions connected to their goals

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -1,10 +1,14 @@
 import Resolutions from "./resolutions";
+import Goals from "../goals/goals";
 
 export default {
   Query: {
     resolutions(obj, args, { userId }) {
       return Resolutions.find({ userId }).fetch();
     }
+  },
+  Resolution: {
+    goals: resolution => Goals.find({ resolutionId: resolution._id }).fetch()
   },
   Mutation: {
     createResolution(obj, { name }, { userId }) {
